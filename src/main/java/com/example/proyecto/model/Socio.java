3a6d696a452ef1c86e5d44306416a69dab43d769
Carlos <carberman2@alu.edu.gva.es>
2026-02-22 12:40:17 +0100

    Comentario Socio

--- src/main/java/com/example/proyecto/model/Socio.java
+++ src/main/java/com/example/proyecto/model/Socio.java
@@ -34,6 +34,7 @@ public class Socio {
     @JsonManagedReference
     private Perfil perfil; 
 
+    // Cascade all para persistir los libros del socio en el Post
     @OneToMany(mappedBy = "socio", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonManagedReference
     private List<Libro> libros = new ArrayList<>();
